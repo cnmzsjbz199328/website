0d857f14f15dab867b702e3c9f0a5d1687fec4aa
cnmzsjbz199328 <tangjiang199328@gmail.com>
2024-08-24 10:09:30 +0930

    refresh 8/24

--- src/section/SectionPictureRight.js
+++ src/section/SectionPictureRight.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Section.css';
 import Post from '../components/Post'; // 更新路径
+import TextToSpeech from '../textToSpeech/TextToSpeech';
 
-function SectionPictureRight({postIdtext, postIdphoto}) {
+function SectionPictureRight({ postIdtext, postIdphoto }) {
+    const [textForSpeech, setTextForSpeech] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    // 用于接收 Post 组件传递的内容
+    const handleContentLoaded = (content) => {
+        setTextForSpeech(content);
+    };
 
     return (
         <div className='section'>
             <div className="text-container">
-            <Post postId={postIdtext} />
+                <Post postId={postIdtext} onContentLoaded={handleContentLoaded} />
+                <TextToSpeech text={textForSpeech} />
             </div>
             <div className="image-container">
                 <Post postId={postIdphoto} />
--- src/volunteering/Volunteering.js
+++ src/volunteering/Volunteering.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import styles from './Volunteering.module.css';
+import SectionPictureLeft from '../section/SectionPictureLeft';
+import SectionPictureRight from '../section/SectionPictureRight';
 
 function Volunteering() {
     return (
-        <div>
-            <h1>Welcome to Our Volunteering</h1>
-            <p>This is the Volunteering page of our site.</p>
+        <div className={styles['Volunteering-container']}>
+            <div className={styles['Volunteering-content']}>
+                <div className={styles['section-container']}>
+                    <SectionPictureLeft postIdphoto={185} postIdtext={188} />
+                </div>
+                <div className={styles['section-container']}>
+                    <SectionPictureRight postIdphoto={190} postIdtext={193} />
+                </div>
+
+                <h1>Some stories from our volunteers</h1>
+                <div className={styles['Story-content']}>
+                    <SectionPictureLeft postIdphoto={199} postIdtext={207} />
+                </div>
+                <div className={styles['section-container']}>
+                    <SectionPictureRight postIdphoto={195} postIdtext={205} />
+                </div>
+                <div className={styles['section-container']}>
+                    <SectionPictureLeft postIdphoto={202} postIdtext={209} />
+                </div>
+            </div>
         </div>
     );
 }
